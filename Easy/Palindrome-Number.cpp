a9f66cf02a23f2721a38b4caaf8ab02828d89147
triangle-motelti <mteltipr03@gmail.com>
2025-11-28 11:29:09 +0100

    Change int to ll for large number handling ; Easy 01

--- Easy/Palindrome-Number.cpp
+++ Easy/Palindrome-Number.cpp
@@ -25,9 +25,9 @@ public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
 
-        int rev = 0, n = x;
+        ll rev = 0, n = x;
         while (x > 0) {
-            int d = x % 10;
+            ll d = x % 10;
             rev = rev * 10 + d;
             x /= 10;
         }
